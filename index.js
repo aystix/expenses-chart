ed578a61f48543ecd2d0fdf0bab2ee270ba03297
adeyemiayomide <aystix2@gmail.com>
2022-11-25 00:30:38 -0800

    another commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var baronee = document.querySelector("#bar1");
+var bartwoo = document.querySelector("#bar2");
+var barthree = document.querySelector("#bar3");
+var barfourr = document.querySelector("#bar4");
+var barfivee = document.querySelector("#bar5");
+var barsixx = document.querySelector("#bar6");
+var barsevenn = document.querySelector("#bar7");
+
+
+baronee.addEventListener("click", () => {
+    document.querySelector("#box-one").style.display = "block";
+})
+bartwoo.addEventListener("click", () => {
+    document.querySelector("#box-two").style.display = "block";
+})
+barthree.addEventListener("click", () => {
+    document.querySelector("#box-three").style.display = "block";
+})
+barfourr.addEventListener("click", () => {
+    document.querySelector("#box-four").style.display = "block";
+})
+barfivee.addEventListener("click", () => {
+    document.querySelector("#box-five").style.display = "block";
+})
+barsixx.addEventListener("click", () => {
+    document.querySelector("#box-six").style.display = "block";
+})
+barsevenn.addEventListener("click", () => {
+    document.querySelector("#box-seven").style.display = "block";
+})
