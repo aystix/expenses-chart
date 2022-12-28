ff2d65513f1ac76ea2a1c025a232cd8979cf8c34
adeyemiayomide <aystix2@gmail.com>
2022-12-28 10:32:10 -0800

    another commit

--- index.js
+++ index.js
@@ -9,22 +9,64 @@ var barsevenn = document.querySelector("#bar7");
 
 baronee.addEventListener("click", () => {
     document.querySelector("#box-one").style.display = "block";
+    document.querySelector("#box-two").style.display = "none";
+    document.querySelector("#box-three").style.display = "none";
+    document.querySelector("#box-four").style.display = "none";
+    document.querySelector("#box-five").style.display = "none";
+    document.querySelector("#box-six").style.display = "none";
+    document.querySelector("#box-seven").style.display = "none";
 })
 bartwoo.addEventListener("click", () => {
     document.querySelector("#box-two").style.display = "block";
+    document.querySelector("#box-one").style.display = "none";
+    document.querySelector("#box-three").style.display = "none";
+    document.querySelector("#box-four").style.display = "none";
+    document.querySelector("#box-five").style.display = "none";
+    document.querySelector("#box-six").style.display = "none";
+    document.querySelector("#box-seven").style.display = "none";
 })
 barthree.addEventListener("click", () => {
     document.querySelector("#box-three").style.display = "block";
+    document.querySelector("#box-two").style.display = "none";
+    document.querySelector("#box-one").style.display = "none";
+    document.querySelector("#box-four").style.display = "none";
+    document.querySelector("#box-five").style.display = "none";
+    document.querySelector("#box-six").style.display = "none";
+    document.querySelector("#box-seven").style.display = "none";
 })
 barfourr.addEventListener("click", () => {
     document.querySelector("#box-four").style.display = "block";
+    document.querySelector("#box-two").style.display = "none";
+    document.querySelector("#box-three").style.display = "none";
+    document.querySelector("#box-one").style.display = "none";
+    document.querySelector("#box-five").style.display = "none";
+    document.querySelector("#box-six").style.display = "none";
+    document.querySelector("#box-seven").style.display = "none";
 })
 barfivee.addEventListener("click", () => {
     document.querySelector("#box-five").style.display = "block";
+    document.querySelector("#box-two").style.display = "none";
+    document.querySelector("#box-three").style.display = "none";
+    document.querySelector("#box-four").style.display = "none";
+    document.querySelector("#box-one").style.display = "none";
+    document.querySelector("#box-six").style.display = "none";
+    document.querySelector("#box-seven").style.display = "none";
 })
 barsixx.addEventListener("click", () => {
     document.querySelector("#box-six").style.display = "block";
+    document.querySelector("#box-two").style.display = "none";
+    document.querySelector("#box-three").style.display = "none";
+    document.querySelector("#box-four").style.display = "none";
+    document.querySelector("#box-five").style.display = "none";
+    document.querySelector("#box-one").style.display = "none";
+    document.querySelector("#box-seven").style.display = "none";
 })
 barsevenn.addEventListener("click", () => {
     document.querySelector("#box-seven").style.display = "block";
+    document.querySelector("#box-two").style.display = "none";
+    document.querySelector("#box-three").style.display = "none";
+    document.querySelector("#box-four").style.display = "none";
+    document.querySelector("#box-five").style.display = "none";
+    document.querySelector("#box-six").style.display = "none";
+    document.querySelector("#box-one").style.display = "none";
 })
